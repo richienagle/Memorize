2b12f2652d9156feb65e3ed05a2e83f2cef83ff2
Rich <richienagle@yahoo.com>
2024-06-23 10:29:18 -0700

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Rich Nagle on 6/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView()
+        }
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Rich Nagle on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
