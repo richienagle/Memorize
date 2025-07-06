cb30ddac20022dcc22b1c9518ea32b594f3bb865
rich <richienagle@gmail.com>
2025-07-06 14:50:40 -0700

    End of Lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,15 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
-        .font(.largeTitle)
         .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius:12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth:2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius:12)
+            }
+        }
     }
 }
 
