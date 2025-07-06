b2db6d67e7a97e331fb2679c58a4de541aadbfdb
rich <richienagle@gmail.com>
2025-07-06 15:18:50 -0700

    created card 'circles'

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack{
+            CardView(isFaceUp: false)
+            CardView()
             CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+            CardView(isFaceUp: true)
         }
         .foregroundColor(.orange)
         .padding()
@@ -22,14 +22,17 @@ struct ContentView: View {
 
 struct CardView: View {
     var isFaceUp: Bool = false
+    
     var body: some View {
         ZStack {
+            //let base = RoundedRectangle(cornerRadius:12)
+            let base = Circle()
             if isFaceUp {
-                RoundedRectangle(cornerRadius:12).foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth:2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth:2)
                 Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius:12)
+                base.fill()
             }
         }
     }
