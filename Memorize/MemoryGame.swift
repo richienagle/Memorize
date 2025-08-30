7d3727ac68e8b22126bd8115fa8243d29c5901b2
Rich <richienagle@gmail.com>
2025-08-30 14:20:56 -0700

    finish lect 5

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -22,20 +22,39 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
         }
     }
     
-    mutating func choose(_ card: Card) {
-        let chosenIndex = index(of:card)
-        cards[chosenIndex].isFaceUp.toggle()
-        //print("chose card \(card)")
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter { index in cards[index].isFaceUp }.only }
+        set { cards.indices.forEach { cards[$0].isFaceUp = (newValue == $0) } }
     }
     
-    func index(of card: Card) -> Int {
-        for index in cards.indices {
-            if cards[index].id == card.id {
-                return index
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: {$0.id == card.id}) {
+            // game logic
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[potentialMatchIndex].content == cards[chosenIndex].content {
+                        cards[chosenIndex].isMatched = true
+                        cards[potentialMatchIndex].isMatched = true
+                    }
+                } else {
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
             }
+            //print("chose card \(card)")
         }
-        return 0  //FIXME: bogus!
+        //do nothing if optional is false
     }
+    
+//    private func index(of card: Card) -> Int? {
+//        for index in cards.indices {
+//            if cards[index].id == card.id {
+//                return index
+//            }
+//        }
+//        return nil
+//        //return 0  //FIXME: bogus!
+//    }
     
     mutating func shuffle() {     // add 'mutating' to modify model
         cards.shuffle()
@@ -53,7 +72,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
         }
          */
         
-        var isFaceUp: Bool =  true
+        var isFaceUp: Bool =  false
         var isMatched: Bool = false
         var content: CardContent
         
@@ -63,4 +82,11 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
         
     }
     
+}
+
+
+extension Array {
+    var only: Element? {
+        count == 1 ? first : nil
+    }
 }
