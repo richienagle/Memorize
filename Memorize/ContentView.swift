f3dd92686304a58681af186c7641537b89194aaf
richie <richienagle@gmail.com>
2026-01-27 17:41:41 -0800

    inital commit,isFaceUp, Card()

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        //'View' is a computed property
+        //VStack(content: {List of Views})
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("😜").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+
+            }
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
