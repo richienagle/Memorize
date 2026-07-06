901b3f9d920b60c327089a50b53e33862e14ea7f
Benjamin <richienagle@gmail.com>
2026-07-06 10:08:00 -0700

    Added .gitignore, basic contentview changes

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
         .padding()
+        .foregroundColor(.orange)
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            }
+            else{
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
+    }
+}
+
+
+
 
 #Preview {
     ContentView()
