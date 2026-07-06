e514e5daf187d93e04eb2e8eb6c0dbe94c940bb2
Benjamin <richienagle@gmail.com>
2026-07-06 09:32:50 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by benjamin on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
