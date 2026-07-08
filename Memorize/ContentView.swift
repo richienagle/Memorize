7c243ea54753959756ec9de0c75565d7858945ad
Benjamin <richienagle@gmail.com>
2026-07-08 09:25:55 -0700

    added foreach for emojis array

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let emojis = ["👻","😈","🤖","👽","😈"]
+
     var body: some View {
         HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content:emojis[index], isFaceUp: true )
+            }
         }
         .padding()
         .foregroundColor(.orange)
@@ -22,7 +23,8 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    @State private var isFaceUp = false
+    var content: String
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack {
@@ -30,7 +32,7 @@ struct CardView: View {
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             }
             else {
                 base.fill()
