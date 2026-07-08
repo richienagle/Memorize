883e50a0287ed130b5e257fb6d04a4ed56cc205d
Benjamin <richienagle@gmail.com>
2026-07-08 09:13:56 -0700

    Added card toggle capability & faceup state

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
+            CardView()
             CardView()
             CardView()
             CardView()
@@ -22,18 +22,23 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State private var isFaceUp = false
     
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
             }
-            else{
-                RoundedRectangle(cornerRadius: 12)
+            else {
+                base.fill()
             }
+        }
+        .onTapGesture {
+            print("tapped")
+            isFaceUp.toggle()
         }
     }
 }
@@ -44,3 +49,4 @@ struct CardView: View {
 #Preview {
     ContentView()
 }
+
